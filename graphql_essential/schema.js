0f3a3eab978dbba2188a645cc154b3d559d5f9a2
Dhiraj Chhetri <dhiraj@mmesolutions.com>
2021-08-17 14:17:07 +0530

    resolvers

--- graphql_essential/schema.js
+++ graphql_essential/schema.js
@@ -6,11 +6,25 @@ const schema = buildSchema ( `
         firstName: String
         lastName:String
         gender:String
-        email:[Email]!
+        email:String
     }
     type Email{
         email:String
     }
+
+    input FriendInput {
+        id:ID
+        firstName: String
+        lastName:String
+        gender:String
+        email:String
+
+    }
+    type Mutation {
+        createFriend(input:FriendInput) :Friend
+    }
+
+
     type Query {
         hello:String
         friend:Friend
